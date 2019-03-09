e374e9f99ba154d575ce4e182f7bc505c94c09be
SakizciAdam <46159571+SakizciAdam@users.noreply.github.com>
2019-03-09 18:17:41 +0300

    Add files via upload

---
+++ SakizciAdamLotsofSwords.cs
@@ -0,0 +1,32 @@
+using Terraria.ModLoader;
+using System;
+using Terraria;
+using SakizciAdamLotsofSwords.Items;
+using Terraria.ID;
+
+
+namespace SakizciAdamLotsofSwords
+{
+	class SakizciAdamLotsofSwords : Mod
+	{
+		public SakizciAdamLotsofSwords()
+		{
+		}
+        public override void AddRecipeGroups()
+        {
+            RecipeGroup group = new RecipeGroup(() => Lang.misc[37] + "MixedSword", new int[]
+            {
+                ItemType("MixedSword"),
+            });
+            RecipeGroup.RegisterGroup("MixedSword", group);
+
+            RecipeGroup grou = new RecipeGroup(() => Lang.misc[37] + "InfiniteBar", new int[]
+            {
+                ItemType("InfiniteBar"),
+            });
+            RecipeGroup.RegisterGroup("InfiniteBar", grou);
+        }
+        
+
+    }
+}
